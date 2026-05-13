c6796056a6f7bc0925117e650f0823f14c098a6c
tigritik <tigritik@gmail.com>
2026-05-12 22:24:10 -0400

    fixed typo

--- src/fsx492.c
+++ src/fsx492.c
@@ -732,7 +732,7 @@ static int writeback_metadata(struct context * ctx)
         if (write_blks(i, 1, blk) < 0) {
             return -EIO;
         }
-        FD_CLR(i, ctx->ditry_map);
+        FD_CLR(i, ctx->dirty_map);
     }
     return 0;
 }
